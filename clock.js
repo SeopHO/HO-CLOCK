e12593cffabf114cbe3d499010681681ed8e2afc
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-12-12 16:52:48 +0900

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,29 @@
+const clock = document.querySelector('.viewClock');
+
+function getTime()
+{
+    let date = new Date();
+
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    clock.innerText = `${detail(hour)}:${detail(min)}:${detail(sec)}`;
+}
+function detail(value)
+{
+    let ret;
+    if(value<10)
+        ret = '0'+value;
+    else
+        ret = value;
+
+    return ret;
+}
+
+function init()
+{
+    setInterval(getTime,1000);
+}
+
+init();
---
+++ whatName.js
@@ -0,0 +1,26 @@
+const form = document.querySelector(".whatName");
+const input = form.querySelector("input");
+
+const USER = "currentUser"
+
+function handleSubmit(event)
+{
+    event.preventDefault();
+}
+
+function askName()
+{
+    form.addEventListener("submit",);
+}
+
+function loadName()
+{
+    
+}
+
+function init()
+{
+    
+}
+
+init();

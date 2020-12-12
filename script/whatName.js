bc5c94499253c32fd87c5413da4b7be20738f910
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-12-13 00:04:08 +0900

    Add files via upload

---
+++ script/whatName.js
@@ -0,0 +1,55 @@
+const form = document.querySelector(".whatName");
+const input = document.querySelector("input");
+
+const viewName = document.getElementById("viewName");
+
+const USER = "currentUser";
+
+
+function askName()
+{   
+    form.addEventListener('submit',handleSubmit);
+}
+
+function saveName(text)
+{
+    localStorage.setItem(USER,text);
+    // showName(currentUser);
+}
+
+function showName(name)
+{
+    form.style.display = 'none';
+    viewName.innerText = `Hello! ${name}`;
+}
+
+function handleSubmit(event)
+{
+    event.preventDefault();
+    const UserValue = input.value;
+   
+    saveName(UserValue);
+    showName(UserValue);
+}
+
+function loadName()
+{
+    const currentUser = localStorage.getItem(USER);
+
+    if(currentUser === null)
+    {
+        askName();
+    }
+    else
+    {
+        showName(currentUser);
+    }
+}
+
+
+function init()
+{
+    loadName();
+}
+
+init();

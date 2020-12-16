33503b0dc7f251a835139114e2ce0c7adeaf6829
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-12-16 23:47:59 +0900

    Add files via upload

--- script/background.js
+++ script/background.js
@@ -1,18 +1,1 @@
-const body = document.querySelector('body');
-const nextBtn = document.querySelector('.right-arrow');
-const beforeBtn = document.querySelector('.left-arrow');
-
-let bgcnt=0;
-
-window.onload = function()
-{
-    let urlString = 'url(images/bg' + bgcnt + '.png)';
-    body.style.backgroundImage = urlString;
-    body.style.backgroundSize = '100% 100%';
-    console.log(false);
-}
-
-function init()
-{   
-    
-}
+let a;

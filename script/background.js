4baaabd8f2dfb5bbe711343affac47180bfce70f
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-12-18 23:57:36 +0900

    Add files via upload

--- script/background.js
+++ script/background.js
@@ -1,1 +1,31 @@
-let a;
+const imageSlier = document.querySelectorAll(".slide");
+
+const arrowPre = document.getElementById("arrow_left");
+const arrowNext = document.getElementById("arrow_right");
+
+let currentValue;
+
+
+function manage()
+{
+    for(let i=0;i<imageSlier.length;i++)
+    {
+        imageSlier[i].style.display = "none";
+        if(i === currentValue)
+        {
+            imageSlier[currentValue].style.display="block";
+            continue;
+        }
+    }
+}
+
+function viewimage(currentValue)
+{
+}
+
+function init()
+{
+
+}
+
+init();

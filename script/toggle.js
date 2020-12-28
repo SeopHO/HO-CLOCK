c2b29934db8f6b6b3e91902c6a180f38688debd3
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-12-28 23:59:18 +0900

    Add files via upload

--- script/toggle.js
+++ script/toggle.js
@@ -1,17 +1,47 @@
 const slide_toggle = document.querySelector(".slide-toggle");
-
+const arrow = document.querySelector(".arrow");
 let slideOnOf=false;
 
 let current_bg_status;
 
 let BG_STATUS = "background status";
 
+let Interval=0;
+let opc;
+
+function fadeIn()
+{
+    Interval = setInterval(showIn,10);
+}
+
+function showIn()
+{
+    opc=0;
+    if(opc<7)
+    {
+        opc++;
+        arrow.style.opacity="0."+opc;
+    }
+    else
+    {
+        clearInterval(Interval);
+    }
+
+}
+
 
 slide_toggle.addEventListener("click",function(){
     if(slideOnOf===false)
+    {
+        fadeIn();
         slideOnOf=true;
+
+    }
     else if(slideOnOf===true)
+    {
         slideOnOf=false;
+
+    }
 
     init();
 
